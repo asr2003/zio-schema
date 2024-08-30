0a62ca82e6e29e46a602804b2d1d85446b435894
asr2003 <162500856+asr2003@users.noreply.github.com>
2024-08-31 01:17:09 +0530

    Update BuildHelper.scala

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,8 +6,6 @@ import BuildInfoKeys.*
 import scalafix.sbt.ScalafixPlugin.autoImport.*
 import scalanativecrossproject.NativePlatform
 
-import scala.scalanative.sbtplugin.ScalaNativePlugin.autoImport.nativeConfig
-
 object BuildHelper {
 
   private val versions: Map[String, String] = {
@@ -27,7 +25,7 @@ object BuildHelper {
   val Scala213: String = versions("2.13")
   val Scala3: String   = versions("3.3")
 
-  val zioVersion                   = "2.1.7"
+  val zioVersion                   = "2.1.9"
   val zioJsonVersion               = "0.7.2"
   val zioPreludeVersion            = "1.0.0-RC28"
   val zioOpticsVersion             = "0.2.2"
@@ -188,7 +186,6 @@ object BuildHelper {
         baseDirectory.value
       )
     },
-    nativeConfig ~= { _.withMultithreading(false) }
   )
 
   def buildInfoSettings(packageName: String) = Seq(
